c46af98c7ba71346801cbda05483128389e9b572
Pana <panagiotis.fotiadis@hotmail.com>
2024-03-06 12:26:16 +0100

    stockfish added and working

--- Bilderkennung/Schachbretterkennungv4.py
+++ Bilderkennung/Schachbretterkennungv4.py
@@ -3,10 +3,25 @@ import cv2
 import numpy as np
 import time
 import utilities
+import subprocess
+from stockfish import Stockfish
+
+stockfish = Stockfish(path="./stockfish/stockfish-windows-x86-64-avx2.exe")
+print(stockfish.is_fen_valid("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+print(stockfish.get_board_visual())
+print(stockfish.get_best_move_time(1000))
+stockfish.make_moves_from_current_position(["b2b4"])
+print(stockfish.get_board_visual())
+stockfish.make_moves_from_current_position(["b7b5"])
+print(stockfish.get_board_visual())
 
 # init camera 
 cap = utilities.init_camera(frame_width=1920, frame_height=1080, exposure=-6.5)
 
+
+
+print("1")
 while True:
     ret, frame = utilities.capture_frame(cap)
     if not ret:
@@ -16,7 +31,7 @@ while True:
     ret, corners = utilities.find_chessboard(frame)
     is_occupied = utilities.calc_average_colors(ret, frame, corners)
 
-    utilities.display_frame()
+    utilities.display_frame(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
