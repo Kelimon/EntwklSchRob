dc06c51581a8091e7c32040882ee8b8dd45ed5d7
Gitarrenprofi <109159609+Gitarrenprofi@users.noreply.github.com>
2024-04-16 14:18:12 +0200

    Halterung Linearmotor v4

--- Arduino Code/Test Drehmechanismus Server/servotest/servotest.ino
+++ Arduino Code/Test Drehmechanismus Server/servotest/servotest.ino
@@ -4,12 +4,12 @@ Stepper Motor(SPU, 3,5,4,6); // Der Schrittmotor erhält die Bezeichnung "Motor"
 
 void setup() //Hier beginnt das Setup.
 {
-Motor.setSpeed(5); // Angabe der Geschwindigkeit in Umdrehungen pro Minute.
+Motor.setSpeed(3); // Angabe der Geschwindigkeit in Umdrehungen pro Minute.
 }
 
 void loop() {
-Motor.step(2048); // Der Motor macht 2048 Schritte, das entspricht einer Umdrehung.
+Motor.step(1048); // Der Motor macht 2048 Schritte, das entspricht einer Umdrehung.
 
-Motor.step(-2048); // Der Motor macht durch das Minuszeichen 2048 Schritte in die andere Richtung.
+Motor.step(-1048); // Der Motor macht durch das Minuszeichen 2048 Schritte in die andere Richtung.
 
 }
