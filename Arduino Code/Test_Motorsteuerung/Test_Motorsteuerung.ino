203f7df7404981bd212913ba96c4aeb6b85db58d
Gitarrenprofi <109159609+Gitarrenprofi@users.noreply.github.com>
2024-04-17 08:52:50 +0200

    Upload Bilder/Bearbeitung Ordnerstruktur

--- Arduino Code/Test_Motorsteuerung/Test_Motorsteuerung.ino
+++ Arduino Code/Test_Motorsteuerung/Test_Motorsteuerung.ino
@@ -1,15 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(10, OUTPUT);
-pinMode(11, OUTPUT);
+pinMode(16, OUTPUT);
+pinMode(17, OUTPUT);
+
 
 pinMode(7, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(4, HIGH);
-digitalWrite(5, LOW);
+digitalWrite(16, HIGH);
+digitalWrite(17, LOW);
 }
