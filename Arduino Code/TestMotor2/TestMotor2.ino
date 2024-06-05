f6f9b7a4b21673224d881e2f7f56374de3ae9768
Gitarrenprofi <109159609+Gitarrenprofi@users.noreply.github.com>
2024-06-05 18:04:23 +0200

    .

--- Arduino Code/TestMotor2/TestMotor2.ino
+++ Arduino Code/TestMotor2/TestMotor2.ino
@@ -1,7 +1,7 @@
-int Motor1a = 4;
-int Motor1b = 5;
-int Motor2a = 6;
-int Motor2b = 7;
+int Motor1a = 6;
+int Motor1b = 7;
+int Motor2a = 4;
+int Motor2b = 5;
 
 
 void setup() {
@@ -10,78 +10,51 @@ void setup() {
   pinMode(Motor1b, OUTPUT);
   pinMode(Motor2a, OUTPUT);
   pinMode(Motor2b, OUTPUT);
-  
-  digitalWrite(Motor2a, HIGH); //Motor 2 einfahren
-  digitalWrite(Motor2b, LOW);
+
   digitalWrite(Motor1a, LOW); //Motor 1 einfahren
   digitalWrite(Motor1b, HIGH);
-  delay(12000);
-  digitalWrite(Motor2a, LOW); //Motor 2 einfahren
+  digitalWrite(Motor2a, HIGH); //Motor 2 einfahren
   digitalWrite(Motor2b, LOW);
-  digitalWrite(Motor1a, LOW); //Motor 1 einfahren
+  delay(11000);
+  digitalWrite(Motor1a, LOW); //Motor 1 stop
   digitalWrite(Motor1b, LOW);
+  digitalWrite(Motor2a, LOW); //Motor 2 stop
+  digitalWrite(Motor2b, LOW);
+
 
 
 
   digitalWrite(Motor1a, HIGH); //Motor 1 ausfahren
   digitalWrite(Motor1b, LOW);
-  delay(6000);
-  digitalWrite(Motor1a, LOW); //Motor ausschalten
+  delay(6200);
+  digitalWrite(Motor1a, LOW); //Motor 1 stop
   digitalWrite(Motor1b, LOW);
-  delay(1000);
+
+  digitalWrite(Motor2a, LOW); //Motor 2 ausfahren
+  digitalWrite(Motor2b, HIGH);
+  delay(2000);
+  digitalWrite(Motor2a, LOW); //Motor 1 stop
+  digitalWrite(Motor2b, LOW);
+
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   /*
   Kopier Vorlage -----------------------------------------------------------------------------------------
-  digitalWrite(Motor1a, HIGH); //Motor 1 ausfahren
-  digitalWrite(Motor1b, LOW);
-  
-  digitalWrite(Motor1a, LOW); //Motor 1 einfahren
-  digitalWrite(Motor1b, HIGH);
-  
   digitalWrite(Motor2a, HIGH); //Motor 2 einfahren
   digitalWrite(Motor2b, LOW);
-  
   digitalWrite(Motor2a, LOW); //Motor 2 ausfahren
   digitalWrite(Motor2b, HIGH);
+  digitalWrite(Motor1a, HIGH); //Motor 1 ausfahren
+  digitalWrite(Motor1b, LOW);
+  digitalWrite(Motor1a, HIGH); //Motor 1 einfahren
+  digitalWrite(Motor1b, LOW);
+
   -----------------------------------------------------------------------------------------------------------
   */
 
-  digitalWrite(Motor1a, LOW);  //Motor 1 einfahren
-  digitalWrite(Motor1b, HIGH);
-  delay(3000);
-  digitalWrite(Motor2a, LOW); //Motor 2 ausfahren
-  digitalWrite(Motor2b, HIGH);
-  delay(4000);
-  digitalWrite(Motor1a, LOW); //Motor 1 aussschalten
-  digitalWrite(Motor1b, LOW);
-  delay(7000);
-  digitalWrite(Motor2a, LOW); //Motor 2 aussschalten
-  digitalWrite(Motor2b, LOW);
-  digitalWrite(Motor1a, HIGH); //Motor 1 ausfahren
-  digitalWrite(Motor1b, LOW);
-  delay(7000);
-  digitalWrite(Motor1a, LOW); //Motor 1 aussschalten
-  digitalWrite(Motor1b, LOW);
-  delay(1000);
-  //------------------------------------------------------------ Alle schritte rückwärts:
-  digitalWrite(Motor1a, LOW);  //Motor 1 einfahren
-  digitalWrite(Motor1b, HIGH);
-  delay(7000);
-  digitalWrite(Motor1a, LOW); //Motor 1 aussschalten
-  digitalWrite(Motor1b, LOW);
-  digitalWrite(Motor2a, HIGH); //Motor 2 einfahren
-  digitalWrite(Motor2b, LOW);
-  delay(7000);
-  digitalWrite(Motor1a, HIGH); //Motor 1 ausfahren
-  digitalWrite(Motor1b, LOW);
-  delay(4000);
-  digitalWrite(Motor2a, LOW); //Motor 2 aussschalten
-  digitalWrite(Motor2b, LOW);
-  delay(1000);
-  digitalWrite(Motor1a, LOW); //Motor 1 aussschalten
-  digitalWrite(Motor1b, LOW);
-  delay(1000);
+
 }
