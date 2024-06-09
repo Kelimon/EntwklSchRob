60da2f28e421a87a04de03b509072e141be9e5b7
Gitarrenprofi <109159609+Gitarrenprofi@users.noreply.github.com>
2024-06-09 22:47:02 +0200

    Studienarbeit

--- Arduino Code/Schrittmotortest/Schrittmotortest.ino
+++ Arduino Code/Schrittmotortest/Schrittmotortest.ino
@@ -1,29 +1,21 @@
-int Index;
+int magneta = 20;
+int magnetb = 21;
+
 
 void setup()
 {
-pinMode(13, OUTPUT); //Enable
-pinMode(12, OUTPUT); //Puls
-pinMode(11, OUTPUT); //Direction
-
-digitalWrite(13,LOW);
-
-digitalWrite(11,HIGH);
-
-for(Index = 0; Index < 1800; Index++)
-{
-digitalWrite(12,HIGH);
-delayMicroseconds(500);
-digitalWrite(12,LOW);
-delayMicroseconds(500);
+pinMode(magneta, OUTPUT);
+pinMode(magnetb, OUTPUT);
 }
 
 
 
 
-}
 
 void loop()
 {
+digitalWrite(magneta, HIGH); //Motor 1 einfahren
+digitalWrite(magneta, LOW); //Motor 1 einfahren
+
 
 }
